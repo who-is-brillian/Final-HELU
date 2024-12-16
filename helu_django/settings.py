7d08fa2acc29417248f8fddec640f8476c75e8de
who-is-brillian <brillianmk86@gmail.com>
2024-12-16 13:13:50 +0700

    final one

--- helu_django/settings.py
+++ helu_django/settings.py
@@ -122,11 +122,7 @@ WSGI_APPLICATION = 'helu_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
